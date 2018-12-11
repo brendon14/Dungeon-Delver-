b426d06cba3b4668869c9ae48a55a413513f85ed
brendon14 <34286219+brendon14@users.noreply.github.com>
2018-12-11 01:53:41 -0500

    Initial commit

---
+++ Assets/__Scripts/IKeyMaster.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public interface IKeyMaster
+{
+    int keyCount { get; set;
+ }                                                // a   
+    int GetFacing();                                                          // b
+}
+
